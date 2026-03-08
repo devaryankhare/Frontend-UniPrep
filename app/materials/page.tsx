1811ce87e8802ae61446919f2f9b941512976fd2
Sohan Rout <sohanrout84@gmail.com>
2026-03-08 10:16:11 +0530

    Feat : added material

---
+++ app/materials/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/ui/Navbar";
+import Notes from "./tabs/notes";
+import FlashCards from "./tabs/flashCards";
+
+export default function Materials() {
+  const [activeTab, setActiveTab] = useState("Flash Cards");
+
+  return (
+    <main>
+      <div>
+        <Navbar />
+      </div>
+
+      <div>
+        {/* Tabs */}
+        <div className="flex gap-4 px-8 pt-6">
+          <button
+            onClick={() => setActiveTab("Flash Cards")}
+            className={`${activeTab === "Flash Cards" ? "bg-linear-to-br from-blue-500 to-blue-600 px-6 py-2 rounded-full text-white duration-300" : "bg-neutral-200 px-6 py-2 rounded-full"}`}
+          >
+            Flash Cards
+          </button>
+
+          <button
+            onClick={() => setActiveTab("Notes")}
+            className={`pb-2 ${activeTab === "Notes" ? "bg-linear-to-br from-blue-500 to-blue-600 px-6 py-2 rounded-full text-white duration-300" : "bg-neutral-200 px-6 py-2 rounded-full"}`}
+          >
+            Notes
+          </button>
+        </div>
+
+        {/* Content */}
+      <div className="p-8">
+        {activeTab === "Notes" && <Notes />}
+        {activeTab === "Flash Cards" && <FlashCards />}
+      </div>
+      </div>
+
+      <div>
+        <Footer />
+      </div>
+    </main>
+  );
+}
