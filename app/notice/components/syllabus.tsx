0af67f5bca9efe236b33d2ed9d65fc10a87b3517
Sohan Rout <sohanrout84@gmail.com>
2026-03-14 22:17:07 +0530

    Feat : added coming soon page

--- app/notice/components/syllabus.tsx
+++ app/notice/components/syllabus.tsx
@@ -1,7 +1,9 @@
+import ComingSoon from "@/app/components/ui/comingSoon";
+
 export default function Syllabus(){
     return(
         <main>
-            
+            <ComingSoon />
         </main>
     );
 }
--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,17 +1,53 @@
+"use client";
+import Blogs from "./components/blogs";
+import Syllabus from "./components/syllabus";
 import Navbar from "../components/ui/Navbar";
 import Footer from "../components/Footer";
+import { useState } from "react";
 
 export default function Notice(){
+    const [activeTab, setActiveTab] = useState<"syllabus" | "blogs">("syllabus");
+
     return(
         <main className="bg-neutral-100">
             <Navbar />
 
             <div className="mx-auto max-w-6xl bg-transparent py-12">
                 <div className="flex items-center justify-center">
-                    <div className="w-fit bg-white flex gap-2 px-2 py-2 rounded-full">
-                        <button className="px-6 py-2 bg-emerald-300 border text-black rounded-full">Syllabus</button>
-                        <button className="px-6 py-2 bg-white text-black rounded-full">Blogs</button>
-                    </div>
+                  <div className="relative w-fit bg-white flex px-2 py-2 rounded-full">
+
+                    {/* Sliding Indicator */}
+                    <div
+                      className={`absolute top-2 bottom-2 w-28 rounded-full bg-emerald-300 border transition-transform duration-300 ease-in-out ${
+                        activeTab === "blogs" ? "translate-x-[112px]" : "translate-x-0"
+                      }`}
+                    />
+
+                    <button
+                      onClick={() => setActiveTab("syllabus")}
+                      className="relative z-10 px-6 py-2 text-black rounded-full"
+                    >
+                      Syllabus
+                    </button>
+
+                    <button
+                      onClick={() => setActiveTab("blogs")}
+                      className="relative z-10 px-6 py-2 text-black rounded-full"
+                    >
+                      Blogs
+                    </button>
+
+                  </div>
+                </div>
+
+                <div className="mt-10">
+                  {activeTab === "syllabus" && (
+                    <Blogs />
+                  )}
+
+                  {activeTab === "blogs" && (
+                    <Syllabus />
+                  )}
                 </div>
             </div>
 
